3d02abe664cbe5962266db7d1c18a59950a55d17
Jhi97 <jeon9771@naver.com>
2021-07-23 12:01:11 +0900

    7/23

---
+++ Baekjoon/Num11720.java
@@ -0,0 +1,24 @@
+package Baekjoon;
+/*N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.*/
+
+import java.util.Scanner;
+
+public class Num11720 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
+        String num = scan.nextLine();
+        String[] split = num.split("");
+        int[] intSplit = new int[split.length];
+        int sum = 0;
+
+        for (int i = 0; i < split.length; i++) {
+            intSplit[i] = Integer.parseInt(split[i]);
+        }
+        for (int i : intSplit) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
