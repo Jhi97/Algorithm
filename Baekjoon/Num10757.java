02efceb5b939471a4cf7ab4f95f61fb24037b672
jhi97 <jeon9771@naver.com>
2021-08-16 19:49:12 +0900

    8/16

---
+++ Baekjoon/Num10757.java
@@ -0,0 +1,18 @@
+package Baekjoon;
+/*두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.*/
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Num10757 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BigInteger A = new BigInteger(scan.next());
+        BigInteger B = new BigInteger(scan.next());
+        scan.close();
+
+        BigInteger result=A.add(B);
+
+        System.out.println(result);
+    }
+}
