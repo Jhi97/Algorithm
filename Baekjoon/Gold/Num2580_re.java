95c0c99c69cae21c93d96d15b145f19df5177ddd
jhi97 <jeon9771@naver.com>
2021-09-23 16:12:01 +0900

    9/23

--- Baekjoon/Gold/Num2580_re.java
+++ Baekjoon/Gold/Num2580_re.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Num2580 {
+public class Num2580_re {
     public static int[][] arr = new int[9][9];
     public static StringBuilder sb = new StringBuilder();
 
